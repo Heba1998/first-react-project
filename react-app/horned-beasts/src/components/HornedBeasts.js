ebdd16faeba27895051b02a276b038d40a51db5d
Heba1998 <hebaalmomani1998@gmail.com>
2021-07-26 23:23:15 +0300

    finish lab 2

--- react-app/horned-beasts/src/components/HornedBeasts.js
+++ react-app/horned-beasts/src/components/HornedBeasts.js
@@ -1,15 +1,45 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card'
 
-class HornedBeasts extends React.Component{
-    render(){
-        return(
-            <div>
-                <h2>{this.props.title}</h2>
-                <img src={this.props.image_url} alt=""></img>
-                <p>{this.props.description}</p>
 
-            </div>
-        )
+class HornedBeast extends React.Component {
+
+constructor(props) {
+    super(props);
+    this.state = {
+        numofvote: 0
     }
 }
-export default HornedBeasts;
+
+increaseNoOfVotes = () => {
+    this.setState({
+        numofvote: this.state.numofvote + 1
+    })
+}
+
+
+render() {
+    return (
+        <div>
+        <Card style={{ width: '18rem' , height: "500px" , padding: "10px" , float: "left", margin: "10px" }}>
+            <Card.Img variant="top" src={this.props.image_url} alt={this.props.title}  style={{height: "250px" }}/>
+            <Card.Body >
+                <Card.Title  >{this.props.title}</Card.Title>
+                <Card.Text>
+                    {this.props.description}
+                </Card.Text>
+                
+                <Card.Text>
+                    votes: {this.state.numofvote}
+                </Card.Text>
+                <Button onClick={this.increaseNoOfVotes}  variant="primary">Vote</Button>
+            </Card.Body>
+        </Card>
+        </div>
+    )
+}
+}
+
+export default HornedBeast
